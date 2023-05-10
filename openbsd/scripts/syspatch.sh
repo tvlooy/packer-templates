7c20363973e05dd5ece62636d57232bb0001563e
Tom Van Looy <tom@ctors.net>
2023-05-10 08:42:21 +0200

    Only apply syspatch when there are patches, and when the kernel is done reordering

--- openbsd/scripts/syspatch.sh
+++ openbsd/scripts/syspatch.sh
@@ -3,5 +3,17 @@
 set -e
 set -x
 
-doas syspatch
+if [[ `doas syspatch -c | wc -l | xargs` -gt 0 ]]; then
 
+  # syspatch: cannot apply patches while reorder_kernel is running
+  while [ true ]; do
+    if pgrep -qxf '/bin/ksh .*reorder_kernel'; then
+      sleep 10
+    else
+      break
+    fi
+  done
+
+  doas syspatch
+
+fi
